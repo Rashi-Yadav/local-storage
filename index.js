d5d27318fae25058f559aa7ee1e8639a288f90f4
Rashi-Yadav <rashi04yadav@gmail.com>
2024-03-17 21:10:27 +0530

    adding delete button to userlist

--- index.js
+++ index.js
@@ -14,12 +14,31 @@ function saveToLocalStorage(event){
   };
 
   localStorage.setItem(userDetails.email, JSON.stringify(userDetails)); 
-
-  function showUserDetails(userDetails) {
+  function showUserDetails() {
     const parent = document.getElementById('userList');
-    const li = document.createElement('li');
-    li.innerHTML = `${userDetails.username} - ${userDetails.email} - ${userDetails.phone}`;
-    parent.appendChild(li);
+    parent.innerHTML = '';
+
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const user = JSON.parse(localStorage.getItem(key));
+
+      const li = document.createElement('li');
+      li.innerHTML = `${user.username} - ${user.email} - ${user.phone}`;
+      parent.appendChild(li);
+
+      // adding delete button
+      const delButton = document.createElement('button');
+      delButton.type = 'button';
+      delButton.textContent = 'Delete';
+
+      // Setting event handler ,to delete user details from list as well as local storage
+      delButton.onclick = () => {
+        parent.removeChild(li);
+        localStorage.removeItem(user.email);
+      };
+      li.appendChild(delButton);
+    }
   }
-  showUserDetails(userDetails);
+
+  showUserDetails();
 }
