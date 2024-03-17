11dc93948ad41c421a6d3071f858a0a049b4b391
Rashi-Yadav <rashi04yadav@gmail.com>
2024-03-17 20:52:41 +0530

    using object to store

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function saveToLocalStorage(event){
   event.preventDefault();
-  const name = event.target.username.value;
-  const email = event.target.emailId.value;
-  const phone = event.target.phonenumber.value;
-  localStorage.setItem('name',name);
-  localStorage.setItem('email',email);
-  localStorage.setItem('phone',phone);  
+  // const name = event.target.username.value;
+  // const email = event.target.email.value;
+  // const phone = event.target.phonenumber.value;
+  // localStorage.setItem('name',name);
+  // localStorage.setItem('email',email);
+  // localStorage.setItem('phone',phone);
+    
+  const userDetails = {
+    username: event.target.username.value,
+    email: event.target.email.value,
+    phone: event.target.phonenumber.value,
+  };
+
+  localStorage.setItem("User Details", JSON.stringify(userDetails));
 }
